5169d9a8f1281332a7bff448d1b13e19ce5e670f
SavliE12 <erika.savli@gmail.com>
2018-01-07 15:36:02 +0100

    Dodani modeli.py

---
+++ modeli.py
@@ -0,0 +1,71 @@
+import sqlite3
+import csv
+
+baza = "PyJet.db"
+con = sqlite3.connect(baza)
+cur = con.cursor()
+cur.execute("PRAGMA foreign_keys = ON")
+
+def poisciIDdestinacije(destinacija):
+    '''vrne ID destinacije, glede na izbrano ime'''
+    cur.execute("""
+        SELECT ID FROM Destinacije WHERE ime = ?
+        """, (destinacija,))
+    return cur.fetchone()
+
+def OdhodnaLetalisca():
+    '''vrne možna odhodna letališča'''
+    cur.execute("""
+    SELECT ime FROM Destinacije
+    """)
+    return cur.fetchall()
+
+def PrihodnaLetalisca(odhodno):
+    '''vrne možna prihodna letališča, glede na izbrano odhodno'''
+    cur.execute("""
+    SELECT ime FROM Destinacije WHERE ID  IN (
+    SELECT Prihod FROM Let WHERE Odhod = ?) """,(poisciIDdestinacije(odhodno)))
+    return cur.fetchall()
+
+def vrniIDleta(prihod,odhod):
+    '''vrne ID izbranega leta''' #odhod in prihod sta ID-ja destinacije
+    cur.execute("""
+    SELECT ID FROM Let
+    WHERE Odhod = ? AND Prihod = ?""",(odhod,prihod))
+    return cur.fetchone()
+
+def vrniIDdrzave(drzava):
+    '''vrne ID drzave'''
+    cur.execute("""
+    SELECT ID FROM Drzava
+    WHERE Ime = ?""", (drzava,))
+    return cur.fetchone()
+
+def dodajPotnika(ime, priimek, emso, drzava):
+    cur.execute("""
+    INSERT INTO Potnik (Ime, Priimek, EMSO, IDdrzave)
+    VALUES (?,?,?,?)
+    """,(ime,priimek,emso,vrniIDdrzave(drzava)[0]))
+    con.commit()    
+
+#spodnji funkciji je potrebno spremeniti, glede na spremembe, ki jih bova naredile na bazi
+    
+##def steviloSedezev(id_leta):
+##    '''vrne st. sedežev na letalu'''
+##    #za preverjanje zasedenosti (če == 0, je potnik obveščen naj izbere drugi let)
+##    cur.execute("""
+##    SELECT Kapaciteta FROM TipLetala
+##    JOIN Let ON TipLetala.ID = Let.Letalo
+##    WHERE Let.ID = ?""", (id_leta,))
+##    return cur.fetchone()
+##
+##def zasediSedez(id_leta):
+##    '''zasede sedez na letu id_leta'''
+
+def urnikInPotnik(id_potnika,id_urnika):
+    '''v tabelo Razpored shrani potnika in njegov izbran let'''
+    cur.execute("""
+    INSERT INTO Razpored (IDpotnika, IDurnika)
+    VALUES (?,?)""", (id_potnika, id_urnika))
+    con.commit()
+    
